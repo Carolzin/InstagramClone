1371e292aeddc8f17bb3094db2b7bce43babdd11
Carolzin <carol-28.01@hotmail.com>
2024-10-07 13:28:08 -0300

    add storys

--- src/components/Header/Layout/index.js
+++ src/components/Header/Layout/index.js
@@ -1,4 +1,5 @@
-
+import { Story } from '../../Story'
+import './styleLayout.css'
 export function Layout() {
 
     return (
@@ -8,18 +9,18 @@ export function Layout() {
 
                 <div className="first-column" style={{gridArea: "firstColumn"}}>
                     <div className="box" >
-                       
+                       <Story />
                     </div>
 
                     <div className="box" style={{margin: "30px 0"}} >
-                        
+                        post
                     </div>
                 </div>
                
 
                 <div style={{ gridArea: "secondColumn" }} >
                     <div className="suggestionBox" >
-                        
+                        sugestao
                     </div>
                 </div>
 
---
+++ src/components/Story/index.js
@@ -0,0 +1,65 @@
+import './style.css';
+
+export function Story() {
+    return (
+        <div className="container">
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://www.eventim.com.br/obj/media/BR-eventim/galery/222x222/e/ed-sheeran-222x222.jpg" alt="Ed Sheeran" />
+                </div>
+                <span>Sheeran</span>
+            </div>
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://64.media.tumblr.com/5fa286178090b3ab35b71dd129c5de11/777611ae5a656e9b-4c/s400x600/7e42eadeac01ce69248daeef2ee90dfe587a48e2.png" alt="Taylor Swift" />
+                </div>
+                <span>Swift13</span>
+            </div>
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://i.scdn.co/image/ab67616100005174e03a98785f3658f0b6461ec4" alt="Olivia Rodrigo" />
+                </div>
+                <span>Olivia Rod</span>
+            </div>
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://64.media.tumblr.com/46bcc8e2b764766687bd36129a669095/8a5f1c372a6ff4fc-b3/s400x600/96b1b4b978a28729c4212b863bb31589b18c29a0.jpg" alt="Harry Styles" />
+                </div>
+                <span>Harry Sty</span>
+            </div>
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://img.wattpad.com/08c5a68da28f0d54ed8d88c6691f3575e6750bea/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f3038754b48787250674541784e673d3d2d3930313730353539302e313631376133316534616162306134663434373234303239363634322e6a7067?s=fit&w=720&h=720" alt="Beyoncé" />
+                </div>
+                <span>Beyoncé</span>
+            </div>
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://64.media.tumblr.com/4d1db1d0dc77e5293ba0288368524827/f0b80d088f2f6983-3e/s1280x1920/bb953abf32ed2c6edd2704f17df5702bfaf3dfad.jpg" alt="Shawn Mendes" />
+                </div>
+                <span>Shawn</span>
+            </div>
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRvpAeQLx0Fq9hK4bAOdIbB2vVPSH2-bnNjA&s" alt="Camila Cabello" />
+                </div>
+                <span>Camila 5H</span>
+            </div>
+
+            <div className="userElemento">
+                <div>
+                    <img className="imagemStory" src="https://64.media.tumblr.com/5d8766e868f074bb162447beb81ee2fa/7a49d0c56e41bd41-cb/s400x600/44ab4dbcacb505def7525fa83a7480b213269891.pnj" alt="Camila Cabello" />
+                </div>
+                <span>Chappell</span>
+            </div>
+
+        </div>
+    );
+}
